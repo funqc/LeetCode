b446c7b2d02e4e27b4976899c049941e37e949d5
funqc <fu92811@163.com>
2015-11-19 18:18:48 +0800

    添加292 Nim Game

---
+++ src/p292/p292.java
@@ -0,0 +1,15 @@
+package p292;
+
+/**
+ * Created by fuqunchao on 15/11/18.
+ */
+public class p292 {
+
+    /*
+    只有当石子个数为4的倍数的时候,先手能赢
+     */
+    public static boolean canWinNim(int n) {
+        if (n % 4 == 0) return false;
+        else return true;
+    }
+}
---
+++ src/test/test.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * Created by fuqunchao on 15/11/18.
+ */
+public class test {
+    public static void main(String[] args){
+        System.out.println("Hello LeetCode!");
+
+        //System.out.println(p292.p292.canWinNim(25));
+    }
+}
